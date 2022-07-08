9dad245302b35e31dc72f5f618a223898c1f8ca1
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-08 13:45:17 -0300

    Encadeamento de then's

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,35 @@
 // Promise creation
 
 const myPromise = new Promise((resolve, reject) => {
+  const nome = "Daniel";
 
-    const nome = "Daniel";
-
-    if(nome === "Daniel") {
-        resolve("Usuário Daniel encontrado")
-    } else {
-        reject("O usuário Daniel não foi encontrado!")
-    }
-})
+  if (nome === "Daniel") {
+    resolve("Usuário Daniel encontrado");
+  } else {
+    reject("O usuário Daniel não foi encontrado!");
+  }
+});
 
 myPromise.then((data) => {
-    console.log(data)
-})
+  console.log(data);
+});
+
+// Encadeamento de Then's
 
 const myPromise2 = new Promise((resolve, reject) => {
+  const nome = "Breno";
 
+  if (nome === "Daniel") {
+    resolve("Usuário Daniel encontrado");
+  } else {
+    reject("Usuário Daniel não encontrado!");
+  }
+});
 
-    const nome = "Breno";
-
-    if(nome === "Daniel") {
-        resolve("Usuário Daniel encontrado")
-    } else {
-        reject("Usuário Daniel não encontrado!")
-    }
-})
-
-myPromise2.then((data) => {
-    console.log(data)
-})
-
+myPromise2
+  .then((data) => {
+    return data.toLowerCase();
+  })
+  .then((stringModified) => {
+    console.log(stringModified);
+  });
