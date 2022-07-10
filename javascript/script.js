f043fcfb6ff3ab4d44c6458c8d300bff37d45ab9
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-10 11:07:21 -0300

    Promisses w race

--- javascript/script.js
+++ javascript/script.js
@@ -58,7 +58,7 @@ myPromise3
 
 const p1 = new Promise((resolve,reject) => {
   setTimeout(() => {
-    resolve("P1 okay! Timeout")
+    resolve("P1 okay! using Timeout")
   }, 2000)
 })
 const p2 = new Promise((resolve,reject) => {
@@ -71,3 +71,23 @@ const p3 = new Promise((resolve,reject) => {
 const resolveAll = Promise.all([p1, p2, p3]).then((data) => {
   console.log(data)
 }) // A promessa é executada de forma assincrona ou seja ela não respeita o fluxo do código
+
+console.log("Depois do all()")
+
+//  Varias promises com race
+
+const p4 = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    resolve("P4 okay! using Timeout")
+  }, 2000)
+})
+const p5 = new Promise((resolve,reject) => {
+  resolve("P5 okay")
+})
+const p6 = new Promise((resolve,reject) => {
+  resolve("P6 okay")
+})
+
+const resolveAllRace = Promise.all([p4, p5, p6]).then((data) => {
+  console.log(data)
+})
