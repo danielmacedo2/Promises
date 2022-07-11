f01470c72bd69f10d53f1ed431cf534548afe743
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-11 11:19:28 -0300

    Fetch request API from GitHub

--- javascript/script.js
+++ javascript/script.js
@@ -53,59 +53,62 @@ myPromise3
     console.log("Erro: " + error);
   });
 
-
 // Resolve all promises
 
-const p1 = new Promise((resolve,reject) => {
+const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("P1 okay! using Timeout")
-  }, 2000)
-})
-const p2 = new Promise((resolve,reject) => {
-  resolve("P2 okay")
-})
-const p3 = new Promise((resolve,reject) => {
-  resolve("P3 okay")
-})
+    resolve("P1 okay! using Timeout");
+  }, 2000);
+});
+const p2 = new Promise((resolve, reject) => {
+  resolve("P2 okay");
+});
+const p3 = new Promise((resolve, reject) => {
+  resolve("P3 okay");
+});
 
 const resolveAll = Promise.all([p1, p2, p3]).then((data) => {
-  console.log(data)
-}) // A promessa é executada de forma assincrona ou seja ela não respeita o fluxo do código
+  console.log(data);
+}); // A promessa é executada de forma assincrona ou seja ela não respeita o fluxo do código
 
-console.log("Depois do all()")
+console.log("Depois do all()");
 
 //  Varias promises com race
 
-const p4 = new Promise((resolve,reject) => {
+const p4 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("P4 okay! using Timeout")
-  }, 2000)
-})
-const p5 = new Promise((resolve,reject) => {
-  resolve("P5 okay")
-})
-const p6 = new Promise((resolve,reject) => {
-  resolve("P6 okay")
-})
+    resolve("P4 okay! using Timeout");
+  }, 2000);
+});
+const p5 = new Promise((resolve, reject) => {
+  resolve("P5 okay");
+});
+const p6 = new Promise((resolve, reject) => {
+  resolve("P6 okay");
+});
 
 const resolveAllRace = Promise.all([p4, p5, p6]).then((data) => {
-  console.log(data)
-})
+  console.log(data);
+});
 
 // Fetch request API from GitHub
 // Fetch API
 
-const userName = "danielmacedo2"
+const userName = "danielmacedo2";
 
 fetch(`https://api.github.com/users/${userName}`, {
   method: "GET",
   headers: {
-    Accept: 'application/vnd.github,v3+json'
-  }
-}).then((response) => {
-  console.log(response)
-  console.log(typeof response)
-  return response.json()
-}).then((data) => {
-  console.log(data)
+    Accept: "application/vnd.github,v3+json",
+  },
 })
+  .then((response) => {
+    console.log(response);
+    console.log(typeof response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+
